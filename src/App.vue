8ab81213fe9af1c39f223e7a1170a0000f8a5859
od317 <osamadrbas31@gmail.com>
2023-06-25 11:05:12 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+    
+        
+                    <div class=" absolute flex justify-between w-full items-center text-white p-[2%] pl-[4%] pr-[5%] z-[5]">
+                          <label for="">
+                               <span class="text-c3 mr-[10%]">9</span>Grids
+                          </label>
+                          <label class="text-[150%]" for="">
+                            <ion-icon name="reorder"></ion-icon>
+                          </label>
+                    </div>    
+          
+                    <div class=" absolute overflow-hidden w-full h-full flex">
+
+
+                             <div class="w-[50%] h-full bg-c1  pt-[5%]">
+                                  
+                                <div class=" text-[300%]  pl-[28%] w-[10%] font-bold p-[10%] text-white">Blvck Tumbler</div>
+                                  
+                                 
+                                   <div class="pb-[20%]  w-[100%] relative text-white">
+
+                                            <div class=" absolute  z-[2]  h-full w-full text-[110%] flex flex-col pt-[2%] pl-[25%]  ">
+                                                <label class="text-[115%] font-bold" for="">Black Lifestyle Lovers.</label>
+                                                <label class="w-[50%] text-[70%] mt-[2%]" for="">Experience what it feels like drinking your favourite beverage out of our premium, all-black mug that was designed with you in mind.</label>
+                                            </div>
+
+                                            <div class=" absolute  z-[1]  pb-[20%] w-[40%] bg-c2"></div>
+                                    
+                                    </div>
+
+                                    <div class="pb-[4%] w-[30%] bg-c3 mt-[10%] relative">
+                                           <div class=" absolute text-white w-[160%] pl-[60%]">EXPLORE NOW</div>
+                                    </div>
+
+                             </div>
+
+
+                             <div class="w-[50%] flex flex-col h-full bg-c2 pt-[5%]">
+                                 
+                                   <div class=" place-self-end z-[3] text-white flex flex-col justify-evenly items-center absolute w-[5%] h-full">
+                                           <label class="text-[200%]" for=""><ion-icon name="arrow-forward"></ion-icon></label>
+                                           <label class="text-c3 box-content border-2 rounded-[50%] relative w-[40%] pb-[40%]" for="">
+                                                <label class=" absolute w-full h-full flex items-center justify-center" for="">
+                                                     <ion-icon class=" " name="arrow-dropright"></ion-icon>
+                                                </label>
+                                           </label>
+                                           <label class="text-[150%]" for=""><ion-icon name="remove"></ion-icon></label>
+                                   </div>
+
+                                   <div class="text-c1 text-[800%] absolute translate-y-[-40%] opacity-50">Blvck Tumbler</div>
+                                   <div class=" text-[300%] opacity-0 pl-[28%] w-[10%] font-bold p-[10%] text-white">Blvck Tumbler</div>
+                              
+                                   <div class="pb-[20%] flex justify-end  w-[100%] relative text-white">
+
+                                            <div class=" absolute  z-[2]  h-full w-full text-[110%] flex flex-col pt-[2%] pl-[50%]  ">
+                                                <label class="text-[115%] font-bold" for="">The design.</label>
+                                                <label class="w-[70%] text-[70%] mt-[2%]" for="">Learn more what makes our Blvck Tumblr unique and different. Let's watch the video !</label>
+                                            </div>
+
+                                            <div class=" absolute  z-[1]  pb-[20%] w-[40%] bg-c1"></div>
+                                    
+                                    </div>
+
+
+                             </div>
+                  
+                    </div>
+    
+                    <div class="w-full h-screen flex items-center justify-center ">
+                            <div class=" relative pb-[37%] w-full  flex   justify-center">
+                                  <img class=" absolute z-[2]  h-[100%]" src="https://iambizi.github.io/Landing-page-Blvck/css/mug.png" alt="">
+                                  <div class=" absolute z-[1] box-content  w-[9%] pb-[8.5%] translate-x-[60%] translate-y-[40%]  border-c3 border-8  justify-center rounded-[50%]"></div>
+                            </div>
+
+                    </div>
+
+
+        
+</template>
+
+
+<style>
+
+
+</style>
